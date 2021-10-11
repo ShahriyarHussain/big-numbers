e5aae527bf5c384914a0ca7a0dae9ed03e4cd98b
ShahriyarHussain <shossainshihab36@gmail.com>
2021-10-11 16:48:55 +0600

    Update Score Logic and Increase Button Size

--- app/src/main/java/com/example/bignumbers/MainActivity.java
+++ app/src/main/java/com/example/bignumbers/MainActivity.java
@@ -10,11 +10,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
-    protected Button leftButton, rightButton;
-    protected TextView scoreText;
-    private int left = 0, right = 0, score = 0;
+    private Button leftButton, rightButton;
+    private TextView scoreText;
+    protected int left = 0, right = 0, score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,56 +28,41 @@ public class MainActivity extends AppCompatActivity {
         scoreText = findViewById(R.id.bottomText);
         this.resetNumbers(leftButton, rightButton);
 
-//        scoreText.setText(score);
-
-        leftButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (getLeft() > getRight()) score++;
-                else score--;
-                resetNumbers(leftButton, rightButton);
-            }
+        leftButton.setOnClickListener(v -> {
+            if (getLeft() > getRight()) this.setScore(this.getScore() + 1);
+            else this.setScore(0);
+            resetNumbers(leftButton, rightButton);
         });
 
-        rightButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (getLeft() > getRight()) score--;
-                else score++;
-                resetNumbers(leftButton, rightButton);
-            }
+        rightButton.setOnClickListener(v -> {
+            if (getLeft() > getRight()) this.setScore(0);
+            else this.setScore(this.getScore() + 1);
+            resetNumbers(leftButton, rightButton);
         });
 
     }
 
     private void resetNumbers(Button leftButton, Button rightButton) {
 
-        if (this.getScore() > 9) this.setScore(0);
-
         this.setLeft((int)((Math.random()) * (1000 - 1 + 1) + 1));
         this.setRight((int)((Math.random()) * (1000 - 1 + 1) + 1));
 
-        leftButton.setText(
-                format("%s", this.left));
-        rightButton.setText(
-                format("%s", this.right));
+        leftButton.setText(format("%s", this.left));
+        rightButton.setText(format("%s", this.right));
 
-        scoreText.setText(format("Score: %d", this.getScore()));
+        if (this.getScore() > 9 && this.getScore() % 10 == 0) {
+            scoreText.setTextSize(16);
+            scoreText.setText(format(Locale.ENGLISH,"Congrats !! You correctly chose %d times !!", this.getScore()));
+        } else {
+            scoreText.setTextSize(26);
+            scoreText.setText(format(Locale.ENGLISH,"Score: %d", this.getScore()));
+        }
     }
 
-    public int getLeft() {
-        return this.left;
-    }
-    public int getRight() {
-        return this.right;
-    }
+    public int getLeft() { return this.left; }
+    public int getRight() { return this.right; }
     public int getScore() { return score; }
-
-    public void setRight(int right) {
-        this.right = right;
-    }
-    public void setLeft(int left) {
-        this.left = left;
-    }
+    public void setRight(int right) { this.right = right; }
+    public void setLeft(int left) { this.left = left; }
     public void setScore(int score) { this.score = score; }
 }
